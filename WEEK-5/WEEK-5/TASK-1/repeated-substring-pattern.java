b43742c7d860e4ed33076962f72a29f08f1891b4
vtu26076-manaswini <vtu26076@veltech.edu.in>
2026-03-27 11:43:38 +0530

    Delete WEEK-5/WEEK-5/TASK-1 directory

--- WEEK-5/WEEK-5/TASK-1/repeated-substring-pattern.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public boolean repeatedSubstringPattern(String s) {
-        String temp = s + s;
-        // Remove first and last character
-        String sub = temp.substring(1, temp.length() - 1);
-        return sub.contains(s);
-    }
-}
